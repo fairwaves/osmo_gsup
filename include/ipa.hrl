887d735ad4a1512bf0df8e1310d682de3249ae69
incrucio <47355057+incrucio@users.noreply.github.com>
2019-06-13 11:53:25 +0300

    Simplified gsup library (#1)

---
+++ include/ipa.hrl
@@ -0,0 +1,13 @@
+-ifndef(IPA).
+-define(IPA, true).
+
+-define(IPAC_PROTO_OSMO, 16#ee).
+-define(IPAC_PROTO_EXT_GSUP, 16#05).
+-define(IPAC_PROTO_IPACCESS, 16#fe).
+-define(IPAC_MSGT_PING, 0).
+-define(IPAC_MSGT_PONG, 1).
+-define(IPAC_MSGT_ID_GET, 4).
+-define(IPAC_MSGT_ID_RESP, 5).
+-define(IPAC_MSGT_ID_ACK, 6).
+
+-endif.
